73eaf9dff83616c5bf6519cd77e51042deccb3a2
Pakhlavon Khamidov <76khwmidov@gmail.com>
2025-08-06 22:31:00 +0500

    netlify commit

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 function Gallery() {
   return (
-      <section className="gallery">
+      <section className="gallery" id="about">
         {Array.from({ length: 14 }, (_, i) => `img/gal-${i + 1}.jpeg`).map((src, index) => (
           <figure key={index} className={`gallery__item gallery__item--${index + 1}`}>
-            <img src={src} alt={`Gallery  ${index + 1}`} className="gallery__img" />
+            <img src={src} alt={`Gallery `} className="gallery__img" />
           </figure>
         ))}
       </section>
